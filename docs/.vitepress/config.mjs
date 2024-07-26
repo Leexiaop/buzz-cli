34a89c46dfbe9b88fbdf444980f8f573e6160826
李晓鹏 <lixiaoeng@zstax.com>
2024-07-26 18:17:11 +0800

    提交配置

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,27 +2,26 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Buzzs-Cli",
-  description: "A Buzzs-Cli websit!",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
+    title: "Buzzs-Cli",
+    description: "A Buzzs-Cli websit!",
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Examples', link: '/markdown-examples' }
+        ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+        sidebar: [
+            {
+                items: [
+                    { text: 'Runtime API Examples', link: '/api-examples' },
+                    { text: 'Markdown Examples', link: '/markdown-examples' },
+                ]
+            }
+        ],
+
+        socialLinks: [
+        { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
         ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+    }
 })
