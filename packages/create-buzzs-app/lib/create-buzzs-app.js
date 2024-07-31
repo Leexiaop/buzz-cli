b164797ea26ffd217d62274442c0a3a06f9a3081
李晓鹏 <lixiaoeng@zstax.com>
2024-07-31 17:47:55 +0800

    提交

--- packages/create-buzzs-app/lib/create-buzzs-app.js
+++ packages/create-buzzs-app/lib/create-buzzs-app.js
@@ -1,10 +1,16 @@
 "use strict";
+const chalk = require("chalk");
 const packageJson = require("../package.json");
 const commander = require("commander");
 
 function init() {
     const program = new commander.Command(packageJson.name)
-        .version(packageJson.version);
+        .version(packageJson.version)
+        .arguments('<project-directory>')
+        .usage(`${chalk.green('<project-directory>')}`)
+        .action(name => {
+            console.log(name)
+        }).parse(process.argv);
 }
 
 module.exports = {
