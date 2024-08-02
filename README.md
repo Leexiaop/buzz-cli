cc02e8df57f95b31f0a787f1f1012bc2ffd96cb1
李晓鹏 <lixiaoeng@zstax.com>
2024-08-02 17:45:28 +0800

    提交eslint

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-创建包的命令：lerna create xxxxxx
+<!-- 创建包的命令：lerna create xxxxxx
 在根目录下添加包，yarn add chalk --ignor-workspace-root-check
 在某个包下安装包：yarn workspace xxxx add xxxx包名
-查看当前的工作空间（包）：yarn workspace info
+查看当前的工作空间（包）：yarn workspace info -->
+
+# Buzzs-cli创建
--- packages/create-buzzs-app/lib/create-buzzs-app.js
+++ packages/create-buzzs-app/lib/create-buzzs-app.js
@@ -3,14 +3,19 @@ const chalk = require('chalk');
 const packageJson = require('../package.json');
 const commander = require('commander');
 
+let projectName = '';
 function init() {
 	const program = new commander.Command(packageJson.name)
 		.version(packageJson.version)
 		.arguments('<project-directory>')
-		.usage(`${chalk.green('<project-directory>')}`)
+		.usage(`${chalk.green('<project-directory>')} [options]`)
 		.action(name => {
-			console.log(name);
-		}).parse(process.argv);
+			projectName = name;
+		})
+		.option('--info', 'print environment debug info')
+		.parse(process.argv);
+	console.log(program.info, 444);
+	console.log(projectName);
 }
 
 module.exports = {
